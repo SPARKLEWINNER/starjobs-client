a106186acd70f1df8bad022e16715b55a20d80f0
Miguel Quintos <eduardo.quintos23@gmail.com>
2022-04-12 17:07:02 +0800

    fix: remove split in onboard form for jobster

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -15,9 +15,7 @@ const isLocalhost = Boolean(
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 )
 
 export function register(config) {
@@ -41,9 +39,10 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          
-           console.log( 'This web app is being served cache-first by a service ' +
-           'worker. To learn more, visit https://cra.link/PWA')
+          console.log(
+            'This web app is being served cache-first by a service ' +
+              'worker. To learn more, visit https://cra.link/PWA',
+          )
         })
       } else {
         // Is not localhost. Just register service worker
@@ -68,10 +67,11 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              
-               console.log( 'New content is available and will be used when all ' +
-               'tabs for this page are closed. See https://cra.link/PWA.'
-          )
+
+              console.log(
+                'New content is available and will be used when all ' +
+                  'tabs for this page are closed. See https://cra.link/PWA.',
+              )
               // Execute callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration)
@@ -80,7 +80,6 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              
 
               // Execute callback
               if (config && config.onSuccess) {
@@ -99,15 +98,12 @@ function registerValidSW(swUrl, config) {
 function checkValidServiceWorker(swUrl, config) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' },
+    headers: {'Service-Worker': 'script'},
   })
     .then((response) => {
       // Ensure service worker exists, and that we really are getting a JS file.
       const contentType = response.headers.get('content-type')
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf('javascript') === -1)
-      ) {
+      if (response.status === 404 || (contentType != null && contentType.indexOf('javascript') === -1)) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then((registration) => {
           registration.unregister().then(() => {
@@ -120,8 +116,7 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      
-        'No internet connection found. App is running in offline mode.'
+      'No internet connection found. App is running in offline mode.'
     })
 }
 

3c3c3cb8faa31e7bdd2e7eb183ad4bc77df51767
Miguel Quintos <eduardo.quintos23@gmail.com>
2022-04-20 20:53:50 +0800

    fix: route issue with slug forgot password link redirect

--- api/src/services/emailTemplates/forgot.template.js
+++ api/src/services/emailTemplates/forgot.template.js
@@ -1,10 +1,15 @@
-
 require('dotenv').config();
 
-const {FRONTEND_URL} = process.env
+const { FRONTEND_URL } = process.env;
 
 const forgotPassword = (token) => {
-    return '<html><body><div style="font-family: inherit; text-align: inherit"><span style="font-family: helvetica, sans-serif; font-size: 18px">You are receiving this because you (or someone else) have requested the reset of the password for your account.&nbsp;</span></div> <div style="font-family: inherit; text-align: inherit"><br></div><div style="font-family: inherit; text-align: inherit"><span style="font-family: helvetica, sans-serif; font-size: 18px">Please click on the following link, or paste this into your browser to complete the process</span><span style="font-family: helvetica, sans-serif; font-size: 18px; color: #0085ff"> </span><a href="'+FRONTEND_URL+'/reset-password/?token='+token+'" title="Link"><span style="font-family: helvetica, sans-serif; font-size: 18px; color: #0085ff">Link</span></a><span style="font-family: helvetica, sans-serif; font-size: 18px">.&nbsp;</span></div> <div style="font-family: inherit; text-align: inherit"><br></div><div style="font-family: inherit; text-align: inherit"><span style="font-family: helvetica, sans-serif; font-size: 18px">If you did not request this, please ignore this email and your password will remain unchanged.</span></div></body></html>'
-}
+    return (
+        '<html><body><div style="font-family: inherit; text-align: inherit"><span style="font-family: helvetica, sans-serif; font-size: 18px">You are receiving this because you (or someone else) have requested the reset of the password for your account.&nbsp;</span></div> <div style="font-family: inherit; text-align: inherit"><br></div><div style="font-family: inherit; text-align: inherit"><span style="font-family: helvetica, sans-serif; font-size: 18px">Please click on the following link, or paste this into your browser to complete the process</span><span style="font-family: helvetica, sans-serif; font-size: 18px; color: #0085ff"> </span><a href="' +
+        FRONTEND_URL +
+        '/reset-password?token=' +
+        token +
+        '" title="Link"><span style="font-family: helvetica, sans-serif; font-size: 18px; color: #0085ff">Link</span></a><span style="font-family: helvetica, sans-serif; font-size: 18px">.&nbsp;</span></div> <div style="font-family: inherit; text-align: inherit"><br></div><div style="font-family: inherit; text-align: inherit"><span style="font-family: helvetica, sans-serif; font-size: 18px">If you did not request this, please ignore this email and your password will remain unchanged.</span></div></body></html>'
+    );
+};
 
-module.exports = forgotPassword
+module.exports = forgotPassword;

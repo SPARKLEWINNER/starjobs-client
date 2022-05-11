9716abeb154b777413efff2686a66b3640487330
Miguel Quintos <eduardo.quintos23@gmail.com>
2022-05-11 12:50:46 +0800

    feat: added app versioning save in databasre

--- api/src/controller/app-settings/index.js
+++ api/src/controller/app-settings/index.js
@@ -1,4 +1,5 @@
 const Ads = require('../../models/Ads');
+const AppSettings = require('../../models/appSettingsSchema');
 
 const logger = require('../../services/logger');
 
@@ -6,19 +7,22 @@ const {BUCKET_URL} = process.env;
 
 var controllers = {
     get_app_settings: async function (req, res) {
-        let ads;
+        let result;
         try {
-            ads = await Ads.find({}).lean().exec();
+            const ads = await Ads.find({}).lean().exec();
+            const appSettings = await AppSettings.find({}).lean().exec()
+
+            result = {
+                ads,
+                appSettings
+            }
         } catch (error) {
             console.error(error);
-            await logger.logError(error, 'Ads.get_ads', null, null, 'GET');
-            return res.status(502).json({success: false, msg: 'Unable to get ads'});
+            await logger.logError(error, 'AppSettings.get_app_settings', null, null, 'GET');
+            return res.status(502).json({success: false, msg: 'Unable to get appSettings'});
         }
 
-        return res.status(200).json({
-            ads,
-            appVersion: '1.0.1'
-        });
+        return res.status(200).json(result);
     },
 };
 

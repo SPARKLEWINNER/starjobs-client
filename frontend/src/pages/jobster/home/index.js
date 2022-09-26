7e9390f604d25dbd606c90144abf8e4a14027d8e
Angelo <angeloc.sparkles@gmail.com>
2022-09-26 14:01:02 +0800

    Added notification permission pop-up after login

--- frontend/src/pages/jobster/home/index.js
+++ frontend/src/pages/jobster/home/index.js
@@ -90,8 +90,56 @@ const Dashboard = () => {
       dynamic: []
     }
   ])
+  const [isEnable, setEnabled] = useState(false)
+
+  const checkIfPushIsEnabled = () => {
+    var ua = navigator.userAgent.toLowerCase()
+    if (ua.indexOf('safari') !== -1) {
+      if (ua.indexOf('chrome') <= -1) return
+    }
+
+    navigator.serviceWorker.ready.then((registration) => {
+      if (!registration.pushManager) {
+        alert('Push Unsupported')
+        return
+      }
+    })
+    //---check if push notification permission has been denied by the user---
+    if (Notification.permission === 'denied') {
+      console.log('User has blocked push notification.')
+      Notification.requestPermission()
+      setEnabled(false)
+      return
+    }
+    if (Notification.permission === 'default') {
+      Notification.requestPermission()
+      setEnabled(false)
+      return
+    }
+
+    //---check if push notification is supported or not---
+    if (!('PushManager' in window)) {
+      alert(`Sorry, Push notification is not supported on this browser.`)
+      return
+    }
+    //---get push notification subscription if serviceWorker is registered and ready---
+    navigator.serviceWorker.ready.then(function (registration) {
+      registration.pushManager
+        .getSubscription()
+        .then(function (subscription) {
+          console.log('subscription', subscription)
+          if (!subscription) return
+
+          console.log(subscription)
+        })
+        .catch(function (error) {
+          console.error('Error occurred enabling push ', error)
+        })
+    })
+  }
 
   useEffect(() => {
+    checkIfPushIsEnabled()
     const load = async () => {
       const result = await gigs_api.get_gigs_client()
       if (!result.ok) {
@@ -116,6 +164,8 @@ const Dashboard = () => {
 
     // eslint-disable-next-line
   }, [])
+
+  useEffect(() => {}, [isEnable])
 
   const checkNotice = (data) => {
     const arrived = data.filter((obj) => obj['status'].includes('Arrived'))

019294a0fae46e6bce22da9c31e4ebbfe8d2a7af
Reymart Montana <rmontana.sparkle@gmail.com>
2023-02-01 13:50:05 +0800

    Fix double posting of gig

--- api/src/controller/users/child-services/clients.service.js
+++ api/src/controller/users/child-services/clients.service.js
@@ -267,20 +267,20 @@ var controllers = {
               preserveNullAndEmptyArrays: true
             }
           },
-          {
-            $lookup: {
-              localField: 'gigs._id',
-              from: 'gigs-histories',
-              foreignField: 'gid',
-              as: 'history'
-            }
-          },
-          {
-            $unwind: {
-              path: '$history',
-              preserveNullAndEmptyArrays: true
-            }
-          },
+          // {
+          //   $lookup: {
+          //     localField: 'gigs._id',
+          //     from: 'gigs-histories',
+          //     foreignField: 'gid',
+          //     as: 'history'
+          //   }
+          // },
+          // {
+          //   $unwind: {
+          //     path: '$history',
+          //     preserveNullAndEmptyArrays: true
+          //   }
+          // },
           {
             $project: {
               _id: 1,
@@ -355,7 +355,7 @@ var controllers = {
                     )
                       .find()
                       .lean()
-
+                      
                     return {
                       ...obj,
                       applicants: history,

edbd737c5fdfd1d7ee52dc5e664ad6620f4b9450
Reymart Montana <rmontana.sparkle@gmail.com>
2023-03-17 15:52:34 +0800

    Gig Nearby Hybrid Notification

--- api/src/common/notifications.js
+++ api/src/common/notifications.js
@@ -1,8 +1,13 @@
 const Notifications = require('../controller/notifications/models/notifications.model')
 const Users = require('../controller/users/models/users.model')
+const FcmTokens = require('../controller/users/models/fcm-tokens')
+const fetch = require("axios");
+const mongoose = require('mongoose')
+const {Types} = mongoose
 
 const logger = require('./loggers')
 
+const {FCM_SERVER_KEY} = process.env
 var controller = {
   globalNotification: async function (additionalData, location) {
     const jobster = await Users.aggregate([
@@ -32,7 +37,7 @@ var controller = {
         isVerified: true
       })
       .exec()
-
+      
     let targetUsers = []
     if (jobster && jobster.length >= 1) {
       await jobster.forEach((item) => {
@@ -40,8 +45,47 @@ var controller = {
       })
     } else {
       return
+    } 
+    const users_fcm = await FcmTokens.find({userId: {$in: targetUsers}})
+    .lean()
+    .exec()
+    const fcmTokenArray = users_fcm.map(user => user.fcmToken);
+    console.log("user tokens: " + fcmTokenArray)
+    if(fcmTokenArray.length != 0 ){
+      try {
+        console.log("--------TRY--------")
+        await fetch('https://fcm.googleapis.com/fcm/send', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'key=' + process.env.FCM_SERVER_KEY
+          },
+          data: {
+            notification: {
+              title: 'Starjobs',
+              body: 'A New Gig is Nearby',
+              imageUrl: 'https://www.starjobs.com.ph/app-logo.png',
+              sound: 1,
+              vibrate: 1,
+              content_available: true,
+              show_in_foreground: true,
+              priority: "high",
+            },
+            data: {
+              url: 'http://192.168.1.3:8000/freelancer/message',
+              type: "route"
+            },
+            registration_ids : fcmTokenArray
+          }
+        })
+      } catch (error) {
+        console.error(error)
+      }
     }
+    
 
+
+    
     const notificationInput = new Notifications({
       title: 'A gig offer nearby is posted',
       body: 'Visit the client to get the gig',

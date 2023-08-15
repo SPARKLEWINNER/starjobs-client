f6e9a8b90d00a430a9eb41778dae7a9eb186e5f0
Reymart Montana <rmontana.sparkle@gmail.com>
2023-08-15 12:46:21 +0800

    Resend sms notif

--- api/src/controller/auth/auth.service.js
+++ api/src/controller/auth/auth.service.js
@@ -260,7 +260,8 @@ var controllers = {
           phone = request_phone
         }
 
-        await sms.send_sms(phone, `Starjobs verification code ${isExisting[0].verificationCode}`)
+        // await sms.send_sms(phone, `Starjobs verification code ${isExisting[0].verificationCode}`)
+        await sms.cast_sms(phone, `Starjobs verification code ${isExisting[0].verificationCode}`)
       } else {
         await mailer.send_mail({email, verifyCode: isExisting[0].verificationCode, type: 'sign_up'})
       }

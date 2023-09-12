53452275174e35342dd42fe4211f1595c1db7ffb
Angelo <angeloc.sparkles@gmail.com>
2023-09-12 14:36:30 +0800

    sound notification

--- api/src/services/fcm-notif.service.js
+++ api/src/services/fcm-notif.service.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose')
 const {ENV} = process.env
 
 var controller = {
-  send_notif: async function (fcmTokenArray, message, url) {
+  send_notif: async function (fcmTokenArray, message, url, status) {
+    let notifSound
+    let channel
+    if (status === 'Applying') {
+      notifSound = 'notification_sound.mp3'
+      channel = 'sound_channel'
+    } else {
+      notifSound = 'default'
+      channel = 'default'
+    }
     await fetch('https://fcm.googleapis.com/fcm/send', {
       method: 'post',
       headers: {
@@ -15,11 +24,12 @@ var controller = {
           title: 'Starjobs',
           body: message,
           icon: 'https://www.starjobs.com.ph/app-logo.png',
-          sound: 1,
+          sound: notifSound,
           vibrate: 1,
           content_available: true,
           show_in_foreground: true,
-          priority: 'high'
+          priority: 'high',
+          android_channel_id: channel
         },
         data: {
           // status:  message[0].status,

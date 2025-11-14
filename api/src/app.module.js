24cf7b7130fa732b4491ddfd090b0f5ec7230940
Reymart Montana <rmontana.sparkle@gmail.com>
2025-11-14 14:27:33 +0800

    Add staging link

--- api/src/app.module.js
+++ api/src/app.module.js
@@ -34,10 +34,8 @@ const allowedOrigins = [
   'https://www.app.starjobs.com.ph',
   'http://localhost:7003', // dev
   'http://localhost:8000',
-  'https://alerts-and-prompts.starjobs-gatsby.pages.dev', // Staging
-  'https://optimize-applicants.starjobs-gatsby.pages.dev', // Staging
-  'https://imgix-integration.starjobs-gatsby.pages.dev', // staging
   'https://home-gig-count.starjobs-gatsby.pages.dev', // staging
+  'https://persist-loc-rate.starjobs-gatsby.pages.dev/', // staging
   'https://sj.spos.site/',
   'https://staging-starjobs.onrender.com/api/internal/v1',
   'https://api-sj.starjobs.com.ph/api/internal/v1'

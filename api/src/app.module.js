620a5eb4c1ec66f6dd3974a96db2996a87751bbc
rmontana2023 <122068863+rmontana2023@users.noreply.github.com>
2025-11-20 11:55:37 +0800

    Update app.module.js

--- api/src/app.module.js
+++ api/src/app.module.js
@@ -34,9 +34,7 @@ const allowedOrigins = [
   'https://www.app.starjobs.com.ph',
   'http://localhost:7003', // dev
   'http://localhost:8000',
-  'https://alerts-and-prompts.starjobs-gatsby.pages.dev', // Staging
-  'https://optimize-applicants.starjobs-gatsby.pages.dev', // Staging
-  'https://imgix-integration.starjobs-gatsby.pages.dev', // staging
+  'https://save-gig-logs.starjobs-gatsby.pages.dev/',
   'https://sj.spos.site/',
   'https://staging-starjobs.onrender.com/api/internal/v1',
   'https://api-sj.starjobs.com.ph/api/internal/v1'

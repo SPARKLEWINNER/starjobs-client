19bc6af336af4496f315a8258de60bba735985bb
Angelo <angeloc.sparkles@gmail.com>
2026-01-07 17:40:09 +0800

    add module holiday

--- api/src/app.module.js
+++ api/src/app.module.js
@@ -46,6 +46,7 @@ const allowedOrigins = [
   'https://jobster-tab.starjobs-gatsby.pages.dev', // staging
   'https://remind-me-later.starjobs-gatsby.pages.dev', // stagin
   'https://merged-ui.starjobs-gatsby.pages.dev', // staging
+  'https://holiday-label.starjobs-gatsby.pages.dev', // staging
   'https://sj.spos.site/',
   'https://staging-starjobs.onrender.com/api/internal/v1',
   'https://api-sj.starjobs.com.ph/api/internal/v1',

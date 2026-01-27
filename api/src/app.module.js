71c6d963a326f41768a9b69b53c658f5ee84c2e3
rmontana2023 <122068863+rmontana2023@users.noreply.github.com>
2026-01-27 14:07:15 +0800

    Add new staging URL for infinite reload

--- api/src/app.module.js
+++ api/src/app.module.js
@@ -47,6 +47,7 @@ const allowedOrigins = [
   'https://remind-me-later.starjobs-gatsby.pages.dev', // stagin
   'https://merged-ui.starjobs-gatsby.pages.dev', // staging
   'https://holiday-label.starjobs-gatsby.pages.dev', // staging
+  'https://infinite-reload.starjobs-gatsby.pages.dev',
   'https://sj.spos.site/',
   'https://staging-starjobs.onrender.com/api/internal/v1',
   'https://api-sj.starjobs.com.ph/api/internal/v1',
